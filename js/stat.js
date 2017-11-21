14efb49c1b23a90edf58cdfe3762e7dda0be1042
Aleksandr <sokolov.ag01@gmail.com>
2017-11-21 17:51:05 +0300

    some fixes

--- js/stat.js
+++ js/stat.js
@@ -1,13 +1,13 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = '#ffffff';
   ctx.fillRect(100, 10, 420, 270);
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(100, 20, 420, 270);
-
-  ctx.fillStyle = '000000';
+  ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура, Вы победили!', 120, 40);
   ctx.fillText('Список результатов', 120, 60);
@@ -37,3 +37,5 @@ for  (var i = 0; i < times.length; i++)
   }
 
 };
+
+renderStatistics();
