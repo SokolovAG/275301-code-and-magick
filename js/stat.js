8d0aa0b928927061941887ee4e536f56bbddee8b
Aleksandr <sokolov.ag01@gmail.com>
2017-11-22 12:06:17 +0300

    some fixes width column height

--- js/stat.js
+++ js/stat.js
@@ -12,30 +12,32 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура, Вы победили!', 120, 40);
   ctx.fillText('Список результатов', 120, 60);
 
+
+  var max = -1;
   var maxIndex = -1;
 
-for  (var i = 0; i < times.length; i++)
-  var time = times[i];
-  if (time > max) {
-    max = time;
-    maxIndex = i;
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+      maxIndex = i;
+    }
+
+    for (var i = 0; i < times.length; i++) {
+
+      var colHeight = 120;
+      var colWidth = 40;
+      var step = colHeight / max;
+      var space = 90;
+      var initialX = 120;
+      var initialY = 80;
+      var lineHeight = 20;
+
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillRect(initialX + colWidth + space * i, initialY, colWidth, times[i] * step);
+      ctx.fillText(names[i], initialX + colWidth + space * i, initialY + 170);
+      ctx.fillText(times[i].toFixed(0), initialX + colWidth + space * i, initialY - 5);
+    }
+
   }
-
-  for  (var i = 0; i < times.length; i++) {
-
-    var histogramHeight = 150;
-    var step = histogramHeight / (max - 0);
-    var colHeight = 150;
-    var colWidth = 40;
-    var space = 50;
-    var initialX = 80;
-    var initialY = 100;
-    var lineHeight = 20;
-
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    ctx.fillRect(initialX + initialY + space * i, times[i] * step, colHeight + colWidth + lineHeight);
-  }
-
 };
-
-renderStatistics();
